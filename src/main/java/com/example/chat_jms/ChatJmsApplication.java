140516632cc8eee6051b3b60690c183189d44019
aldojunior990 <aldojunior990@gmail.com>
2024-10-18 15:15:07 -0300

    First commit

---
+++ src/main/java/com/example/chat_jms/ChatJmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.chat_jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatJmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chat_jms/ChatJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chat_jms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
